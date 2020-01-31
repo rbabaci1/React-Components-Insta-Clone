3ce7b686c63c9042617a66d9c7526b2a00d1915b
rabah babaci <4rabah@gmail.com>
2020-01-31 10:42:56 -0800

    import dummydata to App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
 // import dummydata
 import PostsPage from "./components/PostsContainer/PostsPage";
+import dummydata from "./dummy-data";
 
 const App = () => {
   // set up state for the dummy data and pass to your PostsPage
