3d387d5a7957458c8d6085e2a4d8435130ee56ad
rabah babaci <4rabah@gmail.com>
2020-02-02 12:59:52 -0800

    add some dummy data

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -1,50 +1,102 @@
 const dummyData = [
-    {
+  {
+    username: "philzcoffee",
+    thumbnailUrl:
+      "https://instagram.fsjo9-1.fna.fbcdn.net/vp/d4728e507fe1eb2895fb7ca394bb6431/5E4122B1/t51.2885-15/s150x150/71118783_2434459423505735_6295784028176668008_n.jpg?_nc_ht=instagram.fsjo9-1.fna.fbcdn.net",
+    imageUrl:
+      "https://images.unsplash.com/photo-1477763858572-cda7deaa9bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1876&q=80",
+    likes: 400,
+    timestamp: "July 17th 2017, 12:42:40 pm",
+    comments: [
+      {
         username: "philzcoffee",
-        thumbnailUrl: 'https://instagram.fsjo9-1.fna.fbcdn.net/vp/d4728e507fe1eb2895fb7ca394bb6431/5E4122B1/t51.2885-15/s150x150/71118783_2434459423505735_6295784028176668008_n.jpg?_nc_ht=instagram.fsjo9-1.fna.fbcdn.net',
-        imageUrl: 'https://images.unsplash.com/photo-1477763858572-cda7deaa9bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1876&q=80',
-        likes: 400,
-        timestamp: "July 17th 2017, 12:42:40 pm",
-        comments: [
-            {
-                username: "philzcoffee",
-                text: "We've got more than just delicious coffees to offer at our shops!"
-            },
-            {
-                username: "biancasaurus",
-                text: "Looks delicious!"
-            },
-            {
-                username: "martinseludo",
-                text: "Can't wait to try it!"
-            }
-        ]
-    },
-    {
+        text:
+          "We've got more than just delicious coffees to offer at our shops!"
+      },
+      {
+        username: "biancasaurus",
+        text: "Looks delicious!"
+      },
+      {
+        username: "martinseludo",
+        text: "Can't wait to try it!"
+      }
+    ]
+  },
+  {
+    username: "twitch",
+    thumbnailUrl:
+      "https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg",
+    imageUrl:
+      "https://images.unsplash.com/photo-1566577134770-3d85bb3a9cc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80",
+    likes: 4307,
+    timestamp: "July 15th 2017, 03:12:09 pm",
+    comments: [
+      {
         username: "twitch",
-        thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
-        imageUrl: 'https://images.unsplash.com/photo-1566577134770-3d85bb3a9cc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80',
-        likes: 4307,
-        timestamp: "July 15th 2017, 03:12:09 pm",
-        comments: [
-            {
-                username: "twitch",
-                text: "Epic Street Fighter action here in Las Vegas at #EVO2017!"
-            },
-            {
-                username: "michaelmarzetta",
-                text: "Omg that match was crazy"
-            },
-            {
-                username: "themexican_leprechaun",
-                text: "What a setup"
-            },
-            {
-                username: "dennis_futbol",
-                text: "It that injustice"
-            }
-        ]
-    }
+        text: "Epic Street Fighter action here in Las Vegas at #EVO2017!"
+      },
+      {
+        username: "michaelmarzetta",
+        text: "Omg that match was crazy"
+      },
+      {
+        username: "themexican_leprechaun",
+        text: "What a setup"
+      },
+      {
+        username: "dennis_futbol",
+        text: "It that injustice"
+      }
+    ]
+  },
+  {
+    username: "Lil Wayne",
+    thumbnailUrl: "https://www.rap-up.com/app/uploads/2014/12/lil-wayne.jpg",
+    imageUrl:
+      "https://images.complex.com/complex/images/c_fill,dpr_auto,q_90,w_920/fl_lossy,pg_1/ns9m2ehhozxcg2bf3935/lil-wayne-new-orleans-getty-erika-goldring",
+    likes: 5438400,
+    timestamp: "July 17th 2017, 12:42:40 pm",
+    comments: [
+      {
+        username: "James Bond",
+        text:
+          "We've got more than just delicious coffees to offer at our shops!"
+      },
+      {
+        username: "Drake",
+        text: "Looks delicious!"
+      },
+      {
+        username: "Lil Wayne",
+        text: "Can't wait to try it!"
+      }
+    ]
+  },
+  {
+    username: "Nas",
+    thumbnailUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQLJaynsH-IZbhhRZ1hC9hK4pkdFJ_b447lQIqvRz3FsI3YcHCO",
+    imageUrl:
+      "https://townsquare.media/site/812/files/2014/10/jay-z-nas-2005.jpg?w=980&q=75",
+    likes: 122200,
+    timestamp: "July 17th 2017, 12:42:40 pm",
+    comments: [
+      {
+        username: "philzcoffee",
+        text:
+          "We've got more than just delicious coffees to offer at our shops!"
+      },
+      {
+        username: "biancasaurus",
+        text: "Looks delicious!"
+      },
+      {
+        username: "martinseludo",
+        text: "Can't wait to try it!"
+      }
+    ]
+  }
 ];
 
 export default dummyData;
