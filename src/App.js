eeb60e0c79849470a97efc2c5c2a48b48fbba999
rabah babaci <4rabah@gmail.com>
2020-02-02 14:36:36 -0800

    add search functionality to the search bar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,12 @@ import React, { useState } from "react";
 import "./App.css";
 
 import SearchBar from "./components/SearchBar/SearchBarContainer";
-import PostsPage from "./components/PostsContainer/PostsPage";
 import dummydata from "./dummy-data";
 
 const App = () => {
-  // set up state for the dummy data and pass to your PostsPage
-  const [dummyData, setDummyData] = useState(dummydata);
-
   return (
     <div className="App">
-      <SearchBar />
-      <PostsPage dummyData={dummyData} />
+      <SearchBar dummyData={dummydata} />
     </div>
   );
 };
--- src/components/SearchBar/SearchBarContainer.js
+++ src/components/SearchBar/SearchBarContainer.js
@@ -1,26 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./SearchBar.css";
+import PostsPage from "../PostsContainer/PostsPage";
 
-const SearchBar = () => {
+// Destructure the dummyData array and extract the array
+const SearchBar = ({ dummyData }) => {
+  /* The searchPost state saves the data from the search input
+  on every occurance of the onChange event "line 32" */
+  const [searchedPost, setSearchedPost] = useState("");
+  // The searchResults state is used to set the search result
+  const [searchUsername, setSearchedUsername] = useState([]);
+
+  // useEffect executes whenever the dependency method get changed.
+  useEffect(
+    /* The function to execute as the first argument whenever 
+    the array of dependencies is changed */
+    () => {
+      /* filter the dummyData by comparing the username of every user
+       and the inputted username */
+      const results = dummyData.filter(user =>
+        // return only the object that has the inputted username
+        user.username.toLowerCase().includes(searchedPost.toLowerCase())
+      );
+      // set the state of the searched username to the filter array of users
+      setSearchedUsername(results);
+    },
+
+    /* The array of dependencies that useEffect dependent on which
+    gets changed on every input by the user */
+    [searchedPost]
+  );
+
   return (
-    <div className="search-bar-wrapper">
-      <div className="image-wrapper">
-        <i className="fab fa-instagram" />
+    <div>
+      <div className="search-bar-wrapper">
+        <div className="image-wrapper">
+          <i className="fab fa-instagram" />
+        </div>
+
+        <form className="search-form">
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchedPost}
+            // Using setSearchPost, set the state searchPost to the inputted value from the user
+            onChange={event => setSearchedPost(event.target.value)}
+          />
+        </form>
+
+        <div className="social-wrapper">
+          <div className="social">
+            <i className="far fa-compass" />
+          </div>
+          <div className="social">
+            <i className="far fa-heart" />
+          </div>
+          <div className="social">
+            <i className="far fa-user-circle" />
+          </div>
+        </div>
       </div>
-      <form className="search-form">
-        <input type="text" placeholder="Search" />
-      </form>
-      <div className="social-wrapper">
-        <div className="social">
-          <i className="far fa-compass" />
-        </div>
-        <div className="social">
-          <i className="far fa-heart" />
-        </div>
-        <div className="social">
-          <i className="far fa-user-circle" />
-        </div>
-      </div>
+      {/* render only the posts of the searched username */}
+      <PostsPage dummyData={searchUsername} />
     </div>
   );
 };
